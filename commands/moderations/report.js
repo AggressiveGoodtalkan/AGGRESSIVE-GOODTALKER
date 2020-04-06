4ad2e13379cc2ba32335d2c75e246b50cc751ac3
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2020-04-06 16:41:20 +0800

    bug fixes

--- commands/moderations/report.js
+++ commands/moderations/report.js
@@ -9,28 +9,28 @@ module.exports = {
     usage: "<mention, id>",
     run: async (bot, message, args) => {
         // If the bot can delete the message, do so
-        if (message.deletable) message.delete();
+        if (message.deletable) message.delete({timeout: 5000, reason :"It had to be done."});
 
         // Either a mention or ID
         let rMember = message.mentions.members.first() || message.guild.members.get(args[0]);
 
         // No person found
         if (!rMember)
-            return message.reply("Couldn't find that person?").thenm.delete({timeout: 5000, reason :"It had to be done."});
+            return message.reply("Couldn't find that person?").then(m.delete({timeout: 5000, reason :"It had to be done."}));
 
         // The member has BAN_MEMBERS or is a bot
         if (rMember.hasPermission("BAN_MEMBERS") || rMember.user.bot)
-            return message.channel.send("Can't report that member").then({timeout: 5000, reason :"It had to be done."});
+            return message.channel.send("Can't report that member").then(m.delete({timeout: 5000, reason :"It had to be done."}));
 
         // If there's no argument
         if (!args[1])
-            return message.channel.send("Please provide a reason for the report").then({timeout: 5000, reason :"It had to be done."});
+            return message.channel.send("Please provide a reason for the report").then(m.delete({timeout: 5000, reason :"It had to be done."}));
         
         const channel = message.guild.channels.cache.find(c => c.name === "reports")
             
         // No channel found
         if (!channel)
-            return message.channel.send("Couldn't find a `#reports` channel").then({timeout: 5000, reason :"It had to be done."});
+            return message.channel.send("Couldn't find a `#reports` channel").then(m.delete({timeout: 5000, reason :"It had to be done."}));
 
         const embed = new MessageEmbed()
             .setColor(colors.Red)
