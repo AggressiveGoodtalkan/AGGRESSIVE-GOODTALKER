cb3227b5c29d9e957aacfebc3a9c1cd84468911f
Kaaiitoo <paolo.santos_12@yahoo.com.ph>
2020-04-25 20:47:13 +0800

    fixed say command

--- commands/entertainment/say.js
+++ commands/entertainment/say.js
@@ -7,14 +7,14 @@ module.exports = {
     description: "Says your input via the bot",
     category:"entertainment",
     usage: "<input>",
-    run: (client, message, args) => {
+    run: (bot, message, args) => {
         message.delete();
 
         if (!message.member.hasPermission("MANAGE_MESSAGES"))
-            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
+            return message.reply("You don't have the required permissions to use this command.").then(m => m.delete({timeout: 5000, reason :"It had to be done."}));
 
-        if (args.length < 0)
-            return message.reply("Nothing to say?").then(m => m.delete(5000));
+        if (args.length === 0)
+            return message.reply("Nothing to say?").then(m => m.delete({timeout: 5000, reason :"It had to be done."}));
 
         
         if (args[0].toLowerCase() === "embed") {
