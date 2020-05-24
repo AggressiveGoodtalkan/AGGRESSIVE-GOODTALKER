2aa01e8c7fedcb2771ee461100abe8e2c6a834b9
Lance Selga <lyonecro55@gmail.com>
2020-05-24 13:14:54 +0800

    Add twitch URL to STREAMING presence
    Based from feea6f2e516441fcdb6047e7742262113b5b6853
    Now correctly shows that the bot is "streaming" with the purple color.

--- event_listeners/onReady.js
+++ event_listeners/onReady.js
@@ -4,11 +4,14 @@
 
 module.exports = bot => {
     bot.on('ready', async () => {
-        let logChannel = bot.channels.cache.get('710795359844171797');
-        let logMsg = await `✅ ${bot.user.username} is online on ${bot.guilds.cache.size} server${bot.guilds.cache.size > 1 ? 's' : ''}!`;
+        let logChannel = await bot.channels.cache.get('710795359844171797');
+        let logMsg = `✅ ${bot.user.username} is online on ${bot.guilds.cache.size} server${bot.guilds.cache.size > 1 ? 's' : ''}!`;
 
         console.log(logMsg);
         logChannel.send(logMsg);
-        bot.user.setActivity("AGGRESSIVELY", { type: "STREAMING" });
+        bot.user.setActivity("AGGRESSIVELY | https://discord.gg/ptQ3RE9", {
+            type: "STREAMING",
+            url: "https://www.twitch.tv/aggressive_goodtalkan"
+        });
     });
 };
