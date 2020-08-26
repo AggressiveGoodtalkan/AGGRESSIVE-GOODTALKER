fb55caf0f5e773e1ca64e1e3415b37433f9623b0
Lance Selga <lyonecro55@gmail.com>
2020-08-26 15:19:16 +0800

    Begin streamlining deps (1/?)
    Use only one library (axios as of this commit) for API requests.
    Just to reduce the overall size when installing dependencies.

--- commands/entertainment/chat.js
+++ commands/entertainment/chat.js
@@ -1,4 +1,4 @@
-const axios = require("axios");
+const axios = require("axios").default;
 const colors = require("../../colors.json");
 const { MessageEmbed } = require('discord.js');
 const { getMember } = require('../../functions.js');
@@ -15,7 +15,6 @@ module.exports = {
             return message.channel.send(`❌ ERROR: please put a message!`);
         }
         const toSend = args.join(" ");
-        
         const { data } = await axios.get(`https://some-random-api.ml/chatbot?message=${toSend}`).catch(err => {
             console.log(err);
             return message.channel.send('My servers got overloaded! Please try again in 5 seconds.');
